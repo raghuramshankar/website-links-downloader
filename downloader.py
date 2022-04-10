aab4bc09c59b05bbb1fa99ba6ccc67c2447993f1
Darkside <raghuram.shanakr98@gmail.com>
2022-04-10 11:17:51 +0200

    downloader working

--- downloader.py
+++ downloader.py
@@ -1,4 +1,4 @@
-from distutils.util import subst_vars
+import urllib.request
 import os
 import re
 import html
@@ -8,9 +8,7 @@ import requests
 
 class scraper:
     def __init__(self):
-        self.master_url = (
-            "http://mocha-java.uccs.edu/ECE5720/index.html"
-        )
+        self.master_url = input("Enter URL: ")
         self.master_data = requests.get(
             self.master_url,
             headers={
@@ -25,13 +23,34 @@ class scraper:
             self.master_data.text,
         )
 
+        '''format urls'''
         self.urls = [url + '.mov' for url in self.urls]
-
         self.urls = [html.unescape(url) for url in self.urls]
+        self.urls = [url.replace(" ", "%20") for url in self.urls]
+
+        '''scrape video names'''
+        self.vid_names = re.findall(
+            r"(?<=.mov\">).*(?=<\/a>)",
+            self.master_data.text,
+        )
+
+        '''format names'''
+        self.vid_names = [name.replace(" ", "-") for name in self.vid_names]
+        self.vid_names = [name.replace(".", "_") for name in self.vid_names]
+        self.vid_names = [name.replace(":", "-") for name in self.vid_names]
+        self.vid_names = [name + '.mov' for name in self.vid_names]
 
         print("Total number of URLs available: ", len(self.urls))
+
+    def download_urls(self):
+        for name, link in zip(self.vid_names, self.urls):
+            print("Downloading file: ", name)
+            urllib.request.urlretrieve(link, 'download/'+name)
 
 
 if __name__ == '__main__':
     obj = scraper()
     obj.scrape_urls()
+    obj.download_urls()
+
+    print('Finished download!')
